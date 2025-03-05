8f0d4fe9ca39e54ff18e546b7e0bf22acc2a5134
dlxoho <76989760+dlxoho@users.noreply.github.com>
2025-03-05 23:30:07 +0900

    Time: 1 ms (81.08%), Space: 60.7 MB (8.29%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,6 +3,10 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
+   // 설명 
+   // 1. 0이 아닌값을 전부 앞으로 배치 
+   // 2. 0의 개수부터 0 으로 채워주기 
+
   let max = nums.length;
   let base = 0;
   for (let i=0; i<max; i++) {
