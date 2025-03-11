1488081b3785d448a5fa01caf664b0a95a26598b
dlxoho <76989760+dlxoho@users.noreply.github.com>
2025-03-11 22:31:53 +0900

    Time: 3 ms (27.65%), Space: 54.4 MB (28.38%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -4,22 +4,17 @@
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
-
-    let sArr = s.split("");
-    let tArr = t.split("");
-    let result = sArr.length==0 && tArr.length==0 ? true : false;
-    
-    let max = sArr.length - 1;
+    if (s.length==0 && t.length==0) return true;
+    let max = s.length - 1;
     let idx=0;
-    for (let el of tArr) {
-      let val = sArr[idx];
+    for (let el of t) {
+      let val = s[idx];
       if (el == val) {
         idx++;
       }
       if (idx > max) {
-        result = true;
-        break;
+        return true;
       }
     }
-    return result;
+    return false;
 };
